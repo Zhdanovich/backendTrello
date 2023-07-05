7be5677eb308e1c76374eb3883baad258b8ab8f5
konoplyani4ek <konoplyani4ek@gmail.com>
2023-07-06 02:07:44 +0300

    ticket object extended with new parameters

--- src/main/java/com/example/MyController.java
+++ src/main/java/com/example/MyController.java
@@ -3,13 +3,17 @@ package com.example;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @RestController
 public class MyController {
 
     @GetMapping("/getObject")
     public Ticket getObject() {
-        Ticket myObject = new Ticket();
-        myObject.setTaskBody("do dishes");
+        Ticket myObject = new Ticket("taskBody",1L,"assignedUser","assignor","title",
+                LocalDateTime.now(), "11-11-2011 12:09", "done");
+
         return myObject;
     }
 }
--- src/main/java/com/example/Ticket.java
+++ src/main/java/com/example/Ticket.java
@@ -1,13 +1,38 @@
 package com.example;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Data
+
 public class Ticket {
 
     private String taskBody;
+    private Long id;
+    private String assignedUser;
+    private String assignor;
+    private String title;
+    private LocalDateTime createdTime;
 
+    //expected to be LocalDateFormat, not String
+    private String deadlineDate;
+
+    private String status; // new, in progress, done,etc.
 
     public Ticket() {
+    }
+
+    public Ticket(String taskBody, Long id, String assignedUser, String assignor, String title, LocalDateTime createdTime, String deadlineDate, String status) {
+        this.taskBody = taskBody;
+        this.id = id;
+        this.assignedUser = assignedUser;
+        this.assignor = assignor;
+        this.title = title;
+        this.createdTime = createdTime;
+        this.deadlineDate = deadlineDate;
+        this.status = status;
     }
 }
